c17f2e67b944fc24916602b79537682af7a0743b
tsu2626 <dmwtpdmwtp24@gmail.com>
2018-10-16 01:09:57 +0900

    Cannot read property 'findAll' of undefined

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
+var models = require('../models'); //モデルの読み込み
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  models.User.findAll({
+    order: [
+      ['id', 'desc']
+    ],
+    limit: 10
+  }).then( (users) => {
+    res.render('users', {
+      title: 'Node.js/Express入門: CRUD サンプル',
+      users: users
+    });
+  }); 
+});
+
+router.post('/create', (req, res) => {
+  models.User.create({
+    first_name: req.body.first,
+    last_name: req.body.last_name,
+    email: req.body.email
+  }).then( () => {
+    res.reject('/users');
+  });
 });
 
 module.exports = router;
